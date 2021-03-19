80ab8c892d626d4016dcb0af63d594d7362a3410
Francesca Salvini <franci98sp@gmail.com>
2021-03-19 15:49:59 +0100

    chiedo età e chilometri all'utente

---
+++ script.js
@@ -0,0 +1,31 @@
+//Chiedi all'utente il numero di chilometri che vuole percorrere e la sua età
+
+//Calcola il prezzo del biglietto del treno (0.21 euro al km)
+
+//Calcola lo sconto sul prezzo base del biglietto del treno 
+
+//(20% di sconto ai minorenni e 40% di sconto per gli over 65)
+
+
+//ETÀ UTENTE
+//prompt per sapere gli anni dell'utente 
+
+var etaUtente = prompt ("Quanti anni hai?");
+console.log(etaUtente);
+
+//parseInt per lavorare con numeri interi 
+
+var etaUtenteNumb = parseInt(etaUtente);
+console.log(etaUtenteNumb);
+
+
+//CHILOMETRI UTENTE
+//prompt per sapere quanti chilometri vuole percorrere l'utente
+
+var chilometriUtente = prompt("Quanti chilometri vuoi percorrere?");
+console.log(chilometriUtente);
+
+//parseInt per lavorare con numeri interi
+
+var chilometriUtenteNumb = parseInt(chilometriUtente);
+console.log(chilometriUtenteNumb);
